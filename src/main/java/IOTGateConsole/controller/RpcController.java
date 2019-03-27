7d8c94fe060915f3c97da0caf40598c66137db19
BrianHjj <814837663@qq.com>
2019-03-27 10:01:43 +0800

    与数据库交互

--- src/main/java/IOTGateConsole/controller/RpcController.java
+++ src/main/java/IOTGateConsole/controller/RpcController.java
@@ -1,9 +1,16 @@
 package IOTGateConsole.controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import IOTGateConsole.domain.User;
+import IOTGateConsole.service.TestService1;
 
 @Controller
 @RequestMapping("/rpc")
@@ -11,10 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RpcController {
 	@Value("${zkAddr}")
 	private String zkAddr;
+	@Autowired
+	TestService1 testService1;
+	
 	@RequestMapping("/index")
 	public String initPage(){
-		System.out.println(zkAddr);
-		return "aaa";
+//		name,phone,create_time,age
+		User user=new User();
+		user.setName("326");
+		user.setPhone("326");
+		user.setCreate_time(new Date());
+		user.setAge(1);
+		int i=testService1.insert(user);
+		System.out.println("添加数据，返回值：i"+i);
+		return "index";
 	}
 	
 	
